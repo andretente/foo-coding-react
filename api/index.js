29bd86473d1620ac735327e136d1ca2a5e17bb01
André Tente <andre@kruso.se>
2020-02-12 20:11:27 +0100

    added serving public folder

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import path from 'path'
 
 import express from 'express'
 
@@ -7,10 +8,17 @@ import { firebaseRoutes, giphyRoutes, rickAndMorty } from './routes'
 const app = express()
 const port = process.env.PORT
 
+const publicPath = path.join(__dirname, '../build')
+
+app.use(express.static(publicPath))
+
 app.use(express.json())
 app.use('/firebase', firebaseRoutes)
 app.use('/giphy', giphyRoutes)
 app.use('/rick-and-morty', rickAndMorty)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../build/index.html`))
+})
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server on port ${port}!`))
