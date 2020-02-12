6fe1a0f3b82633468903467f18461cc8a9640322
André Tente <andre@kruso.se>
2020-02-12 19:39:39 +0100

    removing unused packages

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 import 'dotenv/config'
+
 import express from 'express'
 
 import { firebaseRoutes, giphyRoutes, rickAndMorty } from './routes'
 
 const app = express()
+const port = process.env.PORT
+
 app.use(express.json())
 app.use('/firebase', firebaseRoutes)
 app.use('/giphy', giphyRoutes)
 app.use('/rick-and-morty', rickAndMorty)
-const port = 4000
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server on port ${port}!`))
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 import App from './App/App'
 
